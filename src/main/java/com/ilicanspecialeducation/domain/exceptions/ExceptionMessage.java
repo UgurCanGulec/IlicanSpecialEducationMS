738a092599af8ca2b320c649b4cd7f6ceb18cc51
ugurcangulec <gulecugurcan@gmail.com>
2025-03-02 14:38:57 +0300

    Edited heroku yaml

--- src/main/java/com/ilicanspecialeducation/domain/exceptions/ExceptionMessage.java
+++ src/main/java/com/ilicanspecialeducation/domain/exceptions/ExceptionMessage.java
@@ -6,5 +6,4 @@ import lombok.experimental.UtilityClass;
 public class ExceptionMessage {
     public static final String EMPLOYEE_NOT_FOUND = "Personel bilgisi bulunamadı.";
     public static final String POST_NOT_FOUND = "Gönderilere erişim sağlanamadı.";
-    public static final String USER_NOT_FOUND = "Kullanıcı bulunamadı.";
 }
